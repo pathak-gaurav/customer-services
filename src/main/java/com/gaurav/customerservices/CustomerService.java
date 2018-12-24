2c46fc5c10c175938c6c82b6a844f542402ecc4c
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-24 17:29:25 +0530

    Added Service Layer and Fixed Transaction Issue. Closes #2, Closes #5

---
+++ src/main/java/com/gaurav/customerservices/CustomerService.java
@@ -0,0 +1,10 @@
+package com.gaurav.customerservices;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public interface CustomerService {
+}
